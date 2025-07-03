237177226bbe2eda15982a44dfd03c728b70a12f
@fboubekri <faycal.boubekri@vinci-energies.net>
2025-07-03 20:39:59 +0200

    push2

--- dags/dag.py
+++ dags/dag.py
@@ -63,94 +63,94 @@ with DAG(
     tags=['scraping', 'selenium'],
 ) as dag:
 
-#     task_scrape_adzuna = PythonOperator(
-#         task_id='scrape_adzuna_jobs',
-#         python_callable=run_adzuna_scraping,
-#     )
+    task_scrape_adzuna = PythonOperator(
+        task_id='scrape_adzuna_jobs',
+        python_callable=run_adzuna_scraping,
+    )
 
-#     task_scrape_parallel = PythonOperator(
-#         task_id='scrape_all_in_parallel',
-#         python_callable=lambda: parallel_scraping(urls, OUTPUT_DIR),
-#     )
+    task_scrape_parallel = PythonOperator(
+        task_id='scrape_all_in_parallel',
+        python_callable=lambda: parallel_scraping(urls, OUTPUT_DIR),
+    )
 
-#     task_concat = PythonOperator(
-#         task_id='concat_all_csvs',
-#         python_callable=lambda: concat_csv_files(OUTPUT_DIR, FINAL_OUTPUT),
-#     )
+    task_concat = PythonOperator(
+        task_id='concat_all_csvs',
+        python_callable=lambda: concat_csv_files(OUTPUT_DIR, FINAL_OUTPUT),
+    )
 
-#     # task_concat = PythonOperator(
-#     #     task_id='concat_and_save_jobs_to_mongodb',
-#     #     python_callable=lambda: concat_and_save_jobs_csv_to_mongodb(OUTPUT_DIR, "adzuna_jobs"),
-#     # )
+    # task_concat = PythonOperator(
+    #     task_id='concat_and_save_jobs_to_mongodb',
+    #     python_callable=lambda: concat_and_save_jobs_csv_to_mongodb(OUTPUT_DIR, "adzuna_jobs"),
+    # )
 
-#     task_insert_glassdor_to_mongo = PythonOperator(
-#     task_id='insert_glassdor_to_mongodb',
-#         python_callable=lambda: insert_csv_to_mongodb(
-#             "/home/fboubekri/data_challenge/output/all_jobs.csv", 
-#             "glassdoor"
-#         ),
-# )
+    task_insert_glassdor_to_mongo = PythonOperator(
+    task_id='insert_glassdor_to_mongodb',
+        python_callable=lambda: insert_csv_to_mongodb(
+            "/home/fboubekri/data_challenge/output/all_jobs.csv", 
+            "glassdoor"
+        ),
+)
 
 
-#     task_scrape_github_trending = PythonOperator(
-#         task_id='scrape_github_trending',
-#         python_callable=run_github_scraping,
-#     )
+    task_scrape_github_trending = PythonOperator(
+        task_id='scrape_github_trending',
+        python_callable=run_github_scraping,
+    )
 
-#     task_insert_adzuna_to_mongo = PythonOperator(
-#     task_id='insert_adzuna_to_mongodb',
-#         python_callable=lambda: insert_csv_to_mongodb(
-#             "/home/fboubekri/data_challenge/output/adzuna_multi_country.csv", 
-#             "adzuna_jobs"
-#         ),
-# )
+    task_insert_adzuna_to_mongo = PythonOperator(
+    task_id='insert_adzuna_to_mongodb',
+        python_callable=lambda: insert_csv_to_mongodb(
+            "/home/fboubekri/data_challenge/output/adzuna_multi_country.csv", 
+            "adzuna_jobs"
+        ),
+)
 
-#     task_insert_github_to_mongo = PythonOperator(
-#         task_id='insert_github_to_mongodb',
-#         python_callable=lambda: insert_csv_to_mongodb(
-#             "/home/fboubekri/data_challenge/output/github_trending.csv", 
-#             "github_trending"
-#         ),
-# )
-#     task_scrape_meteojob = PythonOperator(
-#         task_id='scrape_meteojob_jobs',
-#         python_callable=run_meteojob_scraping,
-#     )
+    task_insert_github_to_mongo = PythonOperator(
+        task_id='insert_github_to_mongodb',
+        python_callable=lambda: insert_csv_to_mongodb(
+            "/home/fboubekri/data_challenge/output/github_trending.csv", 
+            "github_trending"
+        ),
+)
+    task_scrape_meteojob = PythonOperator(
+        task_id='scrape_meteojob_jobs',
+        python_callable=run_meteojob_scraping,
+    )
 
-#     task_insert_meteojob_to_mongo = PythonOperator(
-#         task_id='insert_meteojob_to_mongodb',
-#         python_callable=lambda: insert_csv_to_mongodb(
-#             "/home/fboubekri/data_challenge/output/meteojob.csv", 
-#             "meteojob"
-#         ),
-# )
-#     task_prepare_google_trends_tasks = PythonOperator(
-#         task_id='prepare_google_trends_tasks',
-#         python_callable=prepare_google_trends_tasks,
-#         provide_context=True,
-#     )
+    task_insert_meteojob_to_mongo = PythonOperator(
+        task_id='insert_meteojob_to_mongodb',
+        python_callable=lambda: insert_csv_to_mongodb(
+            "/home/fboubekri/data_challenge/output/meteojob.csv", 
+            "meteojob"
+        ),
+)
+    task_prepare_google_trends_tasks = PythonOperator(
+        task_id='prepare_google_trends_tasks',
+        python_callable=prepare_google_trends_tasks,
+        provide_context=True,
+    )
 
-#     task_scrape_google_trends_parallel = PythonOperator(
-#         task_id='scrape_google_trends_parallel',
-#         python_callable=scrape_google_trends_parallel,
-#         provide_context=True,
-#     )
+    task_scrape_google_trends_parallel = PythonOperator(
+        task_id='scrape_google_trends_parallel',
+        python_callable=scrape_google_trends_parallel,
+        provide_context=True,
+    )
 
-#     task_insert_google_trend_to_mongo = PythonOperator(
-#         task_id='insert_google_trend_to_mongodb',
-#         python_callable=lambda: insert_csv_to_mongodb(
-#             "/home/fboubekri/data_challenge/output/google_trend.csv", 
-#             "google_trend"
-#         ),
-#     )
+    task_insert_google_trend_to_mongo = PythonOperator(
+        task_id='insert_google_trend_to_mongodb',
+        python_callable=lambda: insert_csv_to_mongodb(
+            "/home/fboubekri/data_challenge/output/google_trend.csv", 
+            "google_trend"
+        ),
+    )
 
-#     task_insert_stepstone_jobs_to_mongo = PythonOperator(
-#         task_id='insert_stepsstone_jobs_to_mongodb',
-#         python_callable=lambda: insert_csv_to_mongodb(
-#             "/home/fboubekri/data_challenge/output/stepstone_jobs.csv", 
-#             "stepstone_jobs"
-#         ),
-#     )
+    task_insert_stepstone_jobs_to_mongo = PythonOperator(
+        task_id='insert_stepsstone_jobs_to_mongodb',
+        python_callable=lambda: insert_csv_to_mongodb(
+            "/home/fboubekri/data_challenge/output/stepstone_jobs.csv", 
+            "stepstone_jobs"
+        ),
+    )
 
 
     etl_task = PythonOperator(
@@ -160,17 +160,17 @@ with DAG(
 
 
 
-    # task_scrape_adzuna >>  task_insert_adzuna_to_mongo
+    task_scrape_adzuna >>  task_insert_adzuna_to_mongo
 
-    # task_prepare_google_trends_tasks >> task_scrape_google_trends_parallel >> task_insert_google_trend_to_mongo
+    task_prepare_google_trends_tasks >> task_scrape_google_trends_parallel >> task_insert_google_trend_to_mongo
 
-    # task_scrape_parallel >> task_concat >> task_insert_glassdor_to_mongo
+    task_scrape_parallel >> task_concat >> task_insert_glassdor_to_mongo
 
-    # task_scrape_github_trending  >> task_insert_github_to_mongo
+    task_scrape_github_trending  >> task_insert_github_to_mongo
 
-    # task_scrape_meteojob >> task_insert_meteojob_to_mongo
+    task_scrape_meteojob >> task_insert_meteojob_to_mongo
 
-    # task_insert_stepstone_jobs_to_mongo
+    task_insert_stepstone_jobs_to_mongo
 
     etl_task
 
